09ce42531a2f24ffb537d698bfd1c8938c943a40
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-08-14 21:10:20 +0000

    Cleaned services tests.

--- test/services/index.js
+++ test/services/index.js
@@ -4,7 +4,7 @@ require('sugar');
 
 var assert = require('assert');
 
-describe('vcap', function () {
+describe('services', function () {
     var client      = require('../client'),
         serviceName = 'test';
 
@@ -33,75 +33,73 @@ describe('vcap', function () {
         return service;
     };
 
-    describe('services', function () {
-        it('get starts empty', function (done) {
-            client.services.get(function (err, services) {
-                assert(! err, err);
-                assert.deepEqual(services, []);
-                done();
-            });
+    it('get starts empty', function (done) {
+        client.services.get(function (err, services) {
+            assert(! err, err);
+            assert.deepEqual(services, []);
+            done();
         });
+    });
 
-        it('get service by name is error', function (done) {
-            client.services.get(serviceName, function (err, services) {
-                assert(err instanceof errors.NotFoundError);
-                done();
-            });
+    it('get service by name is error', function (done) {
+        client.services.get(serviceName, function (err, services) {
+            assert(err instanceof errors.NotFoundError);
+            done();
         });
+    });
 
-        it('create service succeeds', function (done) {
-            client.services.create(serviceName, 'mongodb', '2.4', 'free',
-                function (err) {
+    it('create service succeeds', function (done) {
+        client.services.create(serviceName, 'mongodb', '2.4', 'free',
+            function (err) {
 
-                assert(! err, err);
-                done();
-            });
+            assert(! err, err);
+            done();
         });
+    });
 
-        it('get service by name returns service', function (done) {
-            client.services.get(serviceName, function (err, service) {
-                assert(! err, err);
+    it('get service by name returns service', function (done) {
+        client.services.get(serviceName, function (err, service) {
+            assert(! err, err);
 
-                assert.deepEqual(clean(service), service_);
-                done();
-            });
+            assert.deepEqual(clean(service), service_);
+            done();
         });
+    });
 
-        it('get services includes new service', function (done) {
-            client.services.get(function (err, services) {
-                assert(! err, err);
-                assert.deepEqual(services.map(clean), [ service_ ]);
-                done();
-            });
+    it('get services includes new service', function (done) {
+        client.services.get(function (err, services) {
+            assert(! err, err);
+            assert.deepEqual(services.map(clean), [ service_ ]);
+            done();
         });
+    });
 
-        it('delete service', function (done) {
-            client.services.delete(serviceName, function (err) {
-                assert(! err, err);
-                done();
-            });
+    it('delete service', function (done) {
+        client.services.delete(serviceName, function (err) {
+            assert(! err, err);
+            done();
         });
+    });
 
-        it('delete non-existent service fails', function (done) {
-            client.services.delete(serviceName, function (err) {
-                assert(err instanceof errors.NotFoundError);
-                done();
-            });
+    it('delete non-existent service fails', function (done) {
+        client.services.delete(serviceName, function (err) {
+            assert(err instanceof errors.NotFoundError);
+            done();
         });
+    });
 
-        it('get returns empty', function (done) {
-            client.services.get(function (err, services) {
-                assert(! err, err);
-                assert.deepEqual(services, []);
-                done();
-            });
+    it('get returns empty', function (done) {
+        client.services.get(function (err, services) {
+            assert(! err, err);
+            assert.deepEqual(services, []);
+            done();
         });
+    });
 
-        it('get service by name is error', function (done) {
-            client.services.get(serviceName, function (err, services) {
-                assert(err instanceof errors.NotFoundError);
-                done();
-            });
+    it('get service by name is error', function (done) {
+        client.services.get(serviceName, function (err, services) {
+            assert(err instanceof errors.NotFoundError);
+            done();
         });
     });
 });
