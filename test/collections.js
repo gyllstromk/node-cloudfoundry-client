a457f7b02d55bbb8b466b46be5682671cb80e84b
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-09-03 15:18:41 +0000

    get inners

--- test/collections.js
+++ test/collections.js
@@ -11,6 +11,8 @@ describe('collections', function () {
         requestCallback(object, callback);
     };
 
+    var object = { metadata: { guid: 1 }};
+
     var requestCallback,
         collectionName = 'collection',
         factory    = new Collections(request),
@@ -20,21 +22,77 @@ describe('collections', function () {
         requests.once('request', function (object) {
             assert.deepEqual(object, {
                 endpoint: collectionName,
-                page: 1,
-                method: 'GET'
+                page:     1,
+                method:   'GET'
             });
 
             done();
         });
 
-        requestCallback = function (object, callback) {
-            callback(null, [{ metadata: { guid: 1 }}]);
+        requestCallback = function (object_, callback) {
+            callback(null, [ object ]);
         };
 
         collection.get(function (err, result) {
             assert(! err, err);
-            assert.deepEqual(Object.select(result[0], 'metadata'), { metadata:
-                { guid: 1 }});
+            assert.deepEqual(Object.select(result[0], 'metadata'), object);
+        });
+    });
+
+    describe('single', function () {
+        var result;
+
+        requestCallback = function (object_, callback) {
+            callback(null, object);
+        };
+
+        it('get', function (done) {
+            requests.once('request', function (object) {
+                assert.deepEqual(object, {
+                    endpoint: collectionName + '/1',
+                    page:     null,
+                    method:   'GET'
+                });
+
+                done();
+            });
+
+            collection.get(1, function (err, result_) {
+                assert(! err, err);
+                result = result_;
+                assert.deepEqual(Object.select(result, 'metadata'), object);
+            });
+        });
+
+        it('object has inner1.get methods', function (done) {
+            requests.once('request', function (object) {
+                assert.deepEqual(object, {
+                    endpoint: collectionName + '/1/inner1',
+                    method:   'GET'
+                });
+
+                done();
+            });
+
+            result.inner1.get(function (err, results) {
+                assert(! err, err);
+            });
+        });
+
+        it('object has inner1.put methods', function (done) {
+            requests.once('request', function (object) {
+                assert.deepEqual(object, {
+                    endpoint: collectionName + '/1/inner1',
+                    method:   'PUT',
+                    json: { field: 'value' }
+                });
+
+                done();
+            });
+
+            result.inner1.put({ field: 'value' }, function (err, results) {
+                assert(! err, err);
+            });
         });
     });
 });
